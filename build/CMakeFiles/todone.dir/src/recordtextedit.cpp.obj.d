53e3cc85ac60da28cdeac790d89cd8bbb8bb446b
YirongZhang <yrzhang0821@163.com>
2025-03-13 13:59:19 +0800

    Stop tracking build directory

--- build/CMakeFiles/todone.dir/src/recordtextedit.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/todone.dir/src/recordtextedit.cpp.obj: \
- C:\ZhangYiRong\projs\todone\src\recordtextedit.cpp
--- build/CMakeFiles/todone.dir/src/todaytreewidget.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/todone.dir/src/todaytreewidget.cpp.obj: \
- C:\ZhangYiRong\projs\todone\src\todaytreewidget.cpp
